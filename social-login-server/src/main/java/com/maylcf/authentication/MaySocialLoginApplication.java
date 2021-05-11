dec8b54e706198b18013dcf7e5fbcc85a2ecd693
Mayara Felix <mayaralcabral@gmail.com>
2021-05-11 11:09:37 -0400

    Server initial commit.

---
+++ social-login-server/src/main/java/com/maylcf/authentication/MaySocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.maylcf.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaySocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaySocialLoginApplication.class, args);
+	}
+
+}
